4c9ffce34cb602f4920a1fa06941f6ae79903c88
Francesco Torchia <francescotorchia10@gmail.com>
2024-12-05 21:40:56 +0100

    music fixed

--- js/script.js
+++ js/script.js
@@ -1,46 +1,55 @@
 window.onload = function () {
-    const gameMusic = document.getElementById("game-music");
-    const startButton = document.getElementById("start-button");
-    const restartButton = document.getElementById("restart-button");
-    const difficultyButtons = document.querySelectorAll("#difficulty-selection button");
-    let game;
-    let selectedDifficulty = "Medium"; 
+  const gameStartMusic = document.getElementById("game-start-music");
+  const backgroundMusic = document.getElementById("background-music");
+  const menuMusic = document.getElementById("menu-music"); 
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  const difficultyButtons = document.querySelectorAll("#difficulty-selection button");
+  let game;
+  let selectedDifficulty = "Medium";
+
   
-    
-    difficultyButtons.forEach((button) => {
-      button.addEventListener("click", function () {
-        selectedDifficulty = this.id; 
-        difficultyButtons.forEach((btn) => btn.classList.remove("active"));
-        this.classList.add("active");
-      });
+  gameStartMusic.play();
+  gameStartMusic.volume = 0.5; 
+
+  
+  difficultyButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      selectedDifficulty = this.id;
+      difficultyButtons.forEach((btn) => btn.classList.remove("active"));
+      this.classList.add("active");
     });
-  startButton.addEventListener("click", function () {
-    startGame();
-  });
-  restartButton.addEventListener("click", function () {
-    game.gameEndScreen.style.display = "none";
-    game.startScreen.style.display = "block";  
-    game.audio.currentTime = 0;
-    game.menuAudio.play();
   });
 
   
+  startButton.addEventListener("click", function () {
+    startGame();
+    gameStartMusic.pause();  
+    gameStartMusic.currentTime = 0; 
+  });
+
+  
+  restartButton.addEventListener("click", function () {
+    game.gameEndScreen.style.display = "none";
+    game.startScreen.style.display = "block";
+    backgroundMusic.pause();  
+    backgroundMusic.currentTime = 0; 
+    menuMusic.pause(); 
+    menuMusic.currentTime = 0; 
+    gameStartMusic.play();  
+  });
+
   function startGame() {
     console.log("start game");
     game = new Game();
-
     game.setDifficulty(selectedDifficulty.charAt(0).toUpperCase() + selectedDifficulty.slice(1));
-    
     game.start();
   }
+
+  
   function handleKeydown(event) {
     const key = event.key;
-    const possibleKeystrokes = [
-      "ArrowLeft",
-      "ArrowUp",
-      "ArrowRight",
-      "ArrowDown",
-    ];
+    const possibleKeystrokes = ["ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"];
     if (possibleKeystrokes.includes(key)) {
       event.preventDefault();
       switch (key) {
@@ -59,5 +68,8 @@ window.onload = function () {
       }
     }
   }
+
   window.addEventListener("keydown", handleKeydown);
 };
+
+
